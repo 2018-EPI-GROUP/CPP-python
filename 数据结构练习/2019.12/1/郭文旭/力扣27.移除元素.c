95cad9cbd9b01c7a5dcae4cb1ea1d056d85f91d3
Guo-wenxu <56065097+Guo-wenxu@users.noreply.github.com>
2019-12-01 11:52:51 +0800

    Create 力扣27.移除元素.c

---
+++ 数据结构练习/2019.12/1/郭文旭/力扣27.移除元素.c
@@ -0,0 +1,14 @@
+int removeElement(int* nums, int numsSize, int val){
+    if(numsSize==0)
+    return numsSize;
+    int k=0,i;
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=val)
+        {
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+    return k;
+}
